ae331b23c3fa23b07674a4e22f4b8c7ad9b7f15b
Emirhan Kabakcı <kabakcemirhan@gmail.com>
2025-12-13 18:13:22 +0300

    yeni ödev - hesap makinesi
    dosyaları ayırdım. yeni ödevin hesap makinesi, calculator.java üzerinde çalışman gerekiyor.

---
+++ src/Calculator.java
@@ -0,0 +1,28 @@
+/*
+Kullanıcıdan iki sayı ve bir işlem alıp sonucu hesaplayan bir Java programı yaz.
+1) Kullanıcıdan şu bilgileri al:
+	•	Birinci sayı (double)
+	•	İkinci sayı (double)
+	•	İşlem türü (char veya String): +  -  *  /
+
+2) Sonucu hesapla ve yazdır:
+1. sayıyı gir: 8
+2. sayıyı gir: 4
+işlem seç (+ - * /): /
+sonuç: 2.0
+
+3) Hata kontrolü (zorunlu)
+	•	Kullanıcı / seçerse ve ikinci sayı 0 ise:
+    0'a bölme hatası!
+
+yazdır ve programı güvenli şekilde bitir.
+
+4) Geçersiz işlem kontrolü (zorunlu)
+
+Kullanıcı + - * / dışında bir şey girerse:
+    •	Geçersiz işlem! Lütfen + - * / gir.
+*/
+public class Calculator 
+{
+    
+}
