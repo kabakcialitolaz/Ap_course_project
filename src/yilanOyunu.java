99a3a65b12da3cedf1bba95023a4eef7174c2f9b
Ali Tolaz Kabakcı <kabakcialitolaz@gmail.com>
2026-01-13 21:13:03 +0300

    Update yilanOyunu.java

--- src/yilanOyunu.java
+++ src/yilanOyunu.java
@@ -1,7 +1,22 @@
-public class yilanOyunu 
-{
-    public static void main(String[] args) 
-    {
-        System.out.println("yılan oyunu");    
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+public class yilanOyunu {
+    public static void main(String[] args) {
+        int tahtagenisligi = 600;
+        int tahtauzunlugu = tahtagenisligi;
+        JFrame frame = new JFrame("Yılaaaan") ;
+        frame.setVisible(true);
+        frame.setSize(tahtagenisligi, tahtauzunlugu);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
     }
 }
